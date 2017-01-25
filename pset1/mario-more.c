83e5fb3427fe72715b15c66e0c085f7532c88a97
Bobby Yang <codecheffy@gmail.com>
2017-01-25 15:23:34 -0800

    marios

---
+++ pset1/mario-more.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  int input = 0;
+
+  do {
+    printf("Height: ");
+    input = get_int();
+
+    if(input == 0){
+      return 0;
+    }
+
+  } while (input < 0 || input > 23);
+
+  for(int i = 0; i < input; i++){
+
+    for(int j = 0; j < input - i -1; j++){
+
+      printf("%s", " ");
+
+    }
+
+    for(int k = 0; k < i + 1; k++){
+
+      printf("#");
+
+    }
+
+    printf("%s%s", " ", " ");
+
+    for(int k = 0; k < i + 1; k++){
+
+      printf("#");
+
+    }
+
+    printf("\n");
+
+  }
+}
